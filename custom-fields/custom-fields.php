f7998e20b47507d26688da7754d3cce053f46284
davidegobbi <davidegobbi@localhost.localdomain>
2021-06-28 17:30:53 +0200

    dev

--- custom-fields/custom-fields.php
+++ custom-fields/custom-fields.php
@@ -1,3 +1,3 @@
 <?php
-//require get_template_directory() . '/custom-fields/global-modules/global-modules.php';
+require get_template_directory() . '/custom-fields/global-modules/global-modules.php';
 require get_template_directory() . '/custom-fields/breadcrumbs-categories/breadcrumbs-categories.php';
